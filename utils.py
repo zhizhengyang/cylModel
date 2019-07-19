b30013e32a6138022e3a9f8b187c3e54ea94e916
zhizhengyang <413064943@qq.com>
2019-07-19 13:02:51 +0800

    new

---
+++ utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def gaussian2D(shape=(250,2500), sigma=100):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m+1,-n:n+1]
+
+    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    return h
